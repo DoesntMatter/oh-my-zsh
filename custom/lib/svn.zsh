354809fca304234a968b5f96000be73a734b8a99
DoesntMatter <jaed1@gmx.net>
2012-03-10 10:19:12 +0100

    [30] Improved error handling in custom SVN lib
    Signed-off-by: DoesntMatter <jaed1@gmx.net>

--- custom/lib/svn.zsh
+++ custom/lib/svn.zsh
@@ -1,14 +1,14 @@
 function svn_prompt_rev() {
-    REV=$(svn info | grep 'Revision:' | cut -d" " -f2 2> /dev/null) && echo "$REV"
+    REV=$(svn info 2> /dev/null | grep 'Revision:' | cut -d" " -f2) && echo "$REV"
 }
 
 function svn_prompt_branch() {
-    BRANCH=$(svn info | grep 'URL: ' | cut -d"/" -f4 2> /dev/null) && echo "$BRANCH"
+    BRANCH=$(svn info 2> /dev/null | grep 'URL: ' | cut -d"/" -f4) && echo "$BRANCH"
 }
 
 function svn_repo_behind() {
-    BASE_URL=$(svn info | grep 'URL: ' | cut -d" " -f2 | cut -d"/" -f1,2,3 2> /dev/null)
-    BASE_REV=$(svn info $BASE_URL | grep 'Revision:' | cut -d" " -f2 2> /dev/null)
+    BASE_URL=$(svn info 2> /dev/null | grep 'URL: ' | cut -d" " -f2 | cut -d"/" -f1,2,3)
+    BASE_REV=$(svn info $BASE_URL 2> /dev/null | grep 'Revision:' | cut -d" " -f2)
     
     if [[ $(svn_prompt_rev) < $BASE_REV ]]; then
         echo "$ZSH_THEME_SVN_PROMPT_BEHIND" && return
